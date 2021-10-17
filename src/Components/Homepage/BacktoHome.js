ce35ca79f7260ff9f9d42c3434a623f1aef34ef4
Drishtiishah <shahdrishti2508@gmail.com>
2021-10-17 18:30:57 +0530

    UPDATE

--- src/Components/Homepage/BacktoHome.js
+++ src/Components/Homepage/BacktoHome.js
@@ -5,7 +5,7 @@ import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 export default function TextButton() {
   return (
      
-      <Button align="center" color="primary">
+      <Button align="center" color="primary" style={{color:'#CC011B'}} >
         Back To Top
       <ArrowUpwardIcon size="small"/>
       </Button>
